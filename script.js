d7dd654559b3325a42f2fc13a502c595f91d7e9a
Mntnbassfan <108299929+Mntnbassfan@users.noreply.github.com>
2022-08-28 22:43:56 -0600

    added initial prompt to js

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // variables for passord and options
+  let password = "";
+  let options = {}; 
+
+  function initialPrompt {
+    alert ("You password may may include 8 - 128 characters" + "\n" + 
+    "It may include Upper and Lower case letters" + "\n" 
+    +"It may include Numbers and Special Characters" );
+  }
+}
+generatePassword();
