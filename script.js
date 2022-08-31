a1edd699432adfffd09425c616ce47fd3b1a115c
TuinderJ <joshua.tuinder@gmail.com>
2022-08-30 21:57:05 -0600

    added some comments

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// ********************************************************************** Your closing } is in the wrong place. You can't declare a function inside of another function. See down below for more.
 function generatePassword() {
   //  declared variables for password and options
   let password = "";
@@ -75,6 +76,7 @@ function generatePassword() {
   }
   //  create password
 console.log("test");
+  // ********************************************************************** Don't forget to finish the alphabet
   //   set up array of alphbetical characters
   var alphArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"];
   //   set up arry of special characters
@@ -83,6 +85,10 @@ console.log("test");
   //    set up initial password array
   var initialPassword = [];
 
+
+  // ********************************************************************** Pop wouldn't be a good thing to do here. That method removes the last item from an array and returns it.
+  // **********************************************************************You'd call pop like ... initialPassword.push(alphArray.pop().toUpperCase());
+  // ********************************************************************** This also won't randomize anything. If upperCase is true, every character will be uppercase because it'll check for that first. 
   for (var i = 0; i < options.length; i++) {
   if (options.upperCase) {
     // 
@@ -100,11 +106,14 @@ console.log("test");
         }
 
   console.log(initialPassword);
- 
+//  ********************************************************************** This is where you should put your } to declare the end of your function.
   // shuffle initial password store it to final password
   // found this solution in stack overflow 
   // https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
 
+
+// ********************************************************************** This won't run as it's written. This is only declaring a function, it's not telling it to run. You need to call it in your other function above. It's called like... shuffleArray(initialPassword);
+// ********************************************************************** You're also not using the paramater correctly. Google "How to use a paramater in a function"
   function shuffleArray(initialPassword) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
